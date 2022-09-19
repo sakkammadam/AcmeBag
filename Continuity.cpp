6025aa51ec54781aaf2e701fa88b0385d2f812d1
sakkammadam <sakkammadam@yahoo.com>
2022-09-19 02:40:17 -0700

    SA: Added SQL support - ON CONFLICT DO NOTHING + created new method - insertCustomerTransactions + extended entryMethod

--- Continuity.cpp
+++ Continuity.cpp
@@ -25,27 +25,39 @@ void Continuity::insertCustomerTransactions() {
         // Using the connection object, we will create a transaction to run our SQL instructions against
         pqxx::work txn{cnx};
 
-        nlohmann:json purchaseDetails = getCustomerPurchases();
+        // Let's get all the purchase details
+        nlohmann::json purchaseDetails = getCustomerPurchases();
 
-        // Now we will build the SQL to be inserted using private data members
-        std::string buildSQL =
-                std::string("INSERT INTO CONTINUITY.CUSTOMER_TRANSACTIONS(TRANSACTION_TS,TRANSACTION_DT,CUSTOMER_ID,SKU,UNITS,UNIT_PRICE,TRANSACTION_TOTAL) VALUES(") +
-                "TO_TIMESTAMP(" + "'" + this->getCustomerPurchaseTime() + "'" + "," + "'" + "YYYY-MM-DD HH24:MI:SS" + "'" + ")" + "," +
-                "TO_TIMESTAMP(" + "'" + this->getCustomerPurchaseTime() + "'" + "," + "'" + "YYYY-MM-DD HH24:MI:SS" + "'" + ")::DATE" + "," +
-                std::to_string(this->getCustomerId()) + "," +
-                std::to_string(brandId) + "," +
-                std::to_string(this->getCustomerTotals()) + ")" + ";";
+        // we will need to iterate over the purchase details to understand the components
+        for(nlohmann::json::iterator it = purchaseDetails.begin(); it != purchaseDetails.end(); ++it){
+            std::string sku = it->at("SKU");
+            int units = it->at("UNITS");
+            double unitPrice = it->at("UNIT_PRICE");
+            double txnTotal = unitPrice*units;
 
-        // We will supply the buildSQL and execute within the transaction
-        pqxx::result insertRes{txn.exec(buildSQL)};
+            // Now we will build the SQL to be inserted using private data members
+            std::string buildSQL =
+                    std::string("INSERT INTO CONTINUITY.CUSTOMER_TRANSACTIONS(TRANSACTION_TS,TRANSACTION_DT,CUSTOMER_ID,SKU,UNITS,UNIT_PRICE,TRANSACTION_TOTAL) VALUES(") +
+                    "TO_TIMESTAMP(" + "'" + this->getCustomerPurchaseTime() + "'" + "," + "'" + "YYYY-MM-DD HH24:MI:SS" + "'" + ")" + "," +
+                    "TO_TIMESTAMP(" + "'" + this->getCustomerPurchaseTime() + "'" + "," + "'" + "YYYY-MM-DD HH24:MI:SS" + "'" + ")::DATE" + "," +
+                    std::to_string(this->getCustomerId()) + "," +
+                    "'" + sku + "'" + "," +
+                    std::to_string(units) + "," +
+                    std::to_string(unitPrice) + "," +
+                    std::to_string(txnTotal)  +
+                    ") ON CONFLICT DO NOTHING" + ";";
 
-        if (!insertRes.empty()) {
-            int totCol = insertRes.columns();
-            for (auto row: insertRes) {
-                for (int i = 0; i < totCol; i++) {
-                    std::cout << row[i].c_str() << "|";
+            // We will supply the buildSQL and execute within the transaction
+            pqxx::result insertRes{txn.exec(buildSQL)};
+
+            if (!insertRes.empty()) {
+                int totCol = insertRes.columns();
+                for (auto row: insertRes) {
+                    for (int i = 0; i < totCol; i++) {
+                        std::cout << row[i].c_str() << "|";
+                    }
+                    std::cout << std::endl;
                 }
-                std::cout << std::endl;
             }
         }
         txn.commit();
@@ -56,4 +68,9 @@ void Continuity::insertCustomerTransactions() {
         std::cout << "Query was:; " << e.query() << std::endl;
         std::cerr << "Query was:; " << e.query() << std::endl;
     }
+}
+
+void Continuity::entryMethod() {
+    Bag::entryMethod();
+    insertCustomerTransactions();
 }
