9abe98b7826610b272cb2284488f5151e8253d08
sakkammadam <sakkammadam@yahoo.com>
2022-09-18 22:42:37 -0700

    SA: Commented json prints to console + Used for range to read from vector of objects and run methods.

--- main.cpp
+++ main.cpp
@@ -17,19 +17,28 @@ int main() {
 
     // JSON native file
     std::fstream jsonFile;
-    jsonFile.open("/home/sakkammadam/CLionProjects/cis_554/AcmeBag/sample_mix_bad.json", std::ios::in);
+    jsonFile.open("/home/sakkammadam/CLionProjects/cis_554/AcmeBag/sample.json", std::ios::in);
     nlohmann::json jsonData = nlohmann::json::parse(jsonFile);
-    std::cout << std::setw(4) << jsonData << std::endl;
-    // let's iterate over jsonData using an iterator container
+    //std::cout << std::setw(4) << jsonData << std::endl;
+    // let's iterate over jsonData using an iterator container and load into the vector - acmeBagVector
     for(nlohmann::json::iterator it = jsonData.begin(); it != jsonData.end(); ++it){
         try {
             acmeBaseObj = new Bag(*it);
             acmeBagVector.push_back(acmeBaseObj);
-            acmeBaseObj->display();
+            //acmeBaseObj->display();
         }
         catch(std::runtime_error &runtime_error) {
             std::cout << "Exception occurred: " << runtime_error.what() << std::endl;
         }
+    }
+
+    for(auto item:acmeBagVector){
+        // default customer ids...
+        item->display();
+        // after insertion and retrieval
+        item->entryMethod();
+        // updated customer ids...
+        item->display();
     }
 
     return 0;
