e58ae01b504379fb247bdbfa23bf2390f309ca73
sakkammadam <sakkammadam@yahoo.com>
2022-09-19 17:31:21 -0700

    SA: Added functionality to create NewArmy and Continuity classes based on JSON keys

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "nlohmann/json.hpp"
 #include "Bag.h"
 #include "Continuity.h"
+#include "NewArmy.h"
 
 int main() {
     // let's create a vector to hold our class objects
@@ -18,18 +19,30 @@ int main() {
 
     // JSON native file
     std::fstream jsonFile;
-    jsonFile.open("/home/sakkammadam/CLionProjects/cis_554/AcmeBag/sample_continuity.json", std::ios::in);
+    jsonFile.open("/home/sakkammadam/CLionProjects/cis_554/AcmeBag/sample_newarmy.json", std::ios::in);
     nlohmann::json jsonData = nlohmann::json::parse(jsonFile);
     //std::cout << std::setw(4) << jsonData << std::endl;
     // let's iterate over jsonData using an iterator container and load into the vector - acmeBagVector
     for(nlohmann::json::iterator it = jsonData.begin(); it != jsonData.end(); ++it){
-        try {
-            acmeBaseObj = new Continuity(*it);
-            acmeBagVector.push_back(acmeBaseObj);
-            //acmeBaseObj->display();
-        }
-        catch(std::runtime_error &runtime_error) {
-            std::cout << "Exception occurred: " << runtime_error.what() << std::endl;
+        std::string brandSelection = it->at("BRAND");
+        if(brandSelection == "CONTINUITY") {
+            try {
+                acmeBaseObj = new Continuity(*it);
+                acmeBagVector.push_back(acmeBaseObj);
+                //acmeBaseObj->display();
+            }
+            catch (std::runtime_error &runtime_error) {
+                std::cout << "Exception occurred: " << runtime_error.what() << std::endl;
+            }
+        } else if(brandSelection == "NEW_ARMY"){
+            try {
+                acmeBaseObj = new NewArmy(*it);
+                acmeBagVector.push_back(acmeBaseObj);
+                //acmeBaseObj->display();
+            }
+            catch (std::runtime_error &runtime_error) {
+                std::cout << "Exception occurred: " << runtime_error.what() << std::endl;
+            }
         }
     }
 
